97963ba5666ad1b3eb1e362720654defca3de3fe
albert95gt <albitakacs@gmail.com>
2022-06-05 20:09:04 +0200

    add auth

---
+++ routes/api/auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { validation, ctrlWrapper } = require('../../middlewares');
+const { joiRegisterSchema, joiLoginSchema } = require('../../models/user');
+const { auth: ctrl } = require('../../controllers');
+
+const router = express.Router();
+
+router.post(
+  '/register',
+  validation(joiRegisterSchema),
+  ctrlWrapper(ctrl.register)
+);
+
+router.post('/login', validation(joiLoginSchema), ctrl.login);
+
+module.exports = router;
